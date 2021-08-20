707b85f0223334fcdc42808540fa4956ec3e1589
Alex Collins <alex_collins@intuit.com>
2021-08-20 14:09:59 -0700

    feat: support create

--- cmd/server/root.go
+++ cmd/server/root.go
@@ -8,6 +8,7 @@ import (
 	"github.com/spf13/cobra"
 	"io"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/dynamic"
@@ -26,20 +27,30 @@ import gorillaschema "github.com/gorilla/schema"
 var decoder = gorillaschema.NewDecoder()
 
 func done(w http.ResponseWriter, v interface{}, err error) {
-	w.Header().Set("Content-Type", "application/json")
 	if err != nil {
-		fmt.Printf("500\n")
-		w.WriteHeader(500)
-		_ = json.NewEncoder(io.MultiWriter(os.Stderr, w)).Encode(metav1.Status{
-			Status:  "Failure",
-			Message: err.Error(),
-		})
+		serverError(w, err)
 	} else {
-		fmt.Printf("200\n")
-		w.WriteHeader(200)
-		_ = json.NewEncoder(io.MultiWriter(os.Stdout, w)).Encode(v)
+		ok(w, v)
 	}
 }
+
+func ok(w http.ResponseWriter, v interface{}) {
+	fmt.Printf("200\n")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	_ = json.NewEncoder(io.MultiWriter(os.Stdout, w)).Encode(v)
+}
+
+func serverError(w http.ResponseWriter, err error) {
+	fmt.Printf("500\n")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(500)
+	_ = json.NewEncoder(io.MultiWriter(os.Stderr, w)).Encode(metav1.Status{
+		Status:  "Failure",
+		Message: err.Error(),
+	})
+}
+
 func NewCommand() *cobra.Command {
 	var kubeconfig string
 	var namespace string
@@ -66,7 +77,7 @@ func NewCommand() *cobra.Command {
 			http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 				parts := strings.Split(r.URL.Path, "/")
 				query := r.URL.Query()
-				fmt.Printf("%q %q\n", parts, query)
+				fmt.Printf("%s %q (%d) %q\n", r.Method, parts, len(parts), query)
 				switch parts[1] {
 				case "apis":
 					switch len(parts) {
@@ -77,33 +88,41 @@ func NewCommand() *cobra.Command {
 						groupVersion, err := disco.ServerResourcesForGroupVersion(parts[2] + "/" + parts[3])
 						done(w, groupVersion, err)
 					case 7:
+						switch r.Method {
+						case "GET":
+							clusterName, namespace := split(parts[5])
+							list(w, r, parts[2], parts[3], clusterName, namespace, parts[6], clients[clusterName])
+						case "POST":
+							clusterName, namespace := split(parts[5])
+							create(w, r, parts[2], parts[3], clusterName, namespace, parts[6], clients[clusterName])
+						default:
+							serverError(w, fmt.Errorf("unexpected method"))
+						}
+					case 8:
 						clusterName, namespace := split(parts[5])
-						list(w, r, parts[2], parts[3], namespace, parts[6], clients[clusterName])
+						get(w, r, parts[2], parts[3], clusterName, namespace, parts[6], parts[7], clients[clusterName])
 					default:
-						done(w, nil, fmt.Errorf("unexpected number of path parts"))
+						serverError(w, fmt.Errorf("unexpected number of path parts"))
 					}
 				default:
 					switch len(parts) {
-					case 2: // /api
-						v := metav1.APIVersions{TypeMeta: metav1.TypeMeta{Kind: "APIVersions"}, Versions: []string{}}
-						done(w, v, nil)
-					case 3: // api/version -- legacy, usually v1
+					case 2:
+						ok(w, metav1.APIVersions{TypeMeta: metav1.TypeMeta{Kind: "APIVersions"}, Versions: []string{}})
+					case 3:
 						list, err := disco.ServerResourcesForGroupVersion(parts[2])
 						done(w, list, err)
 					case 6:
-						clusterName, namespace := split(parts[5])
-						list(w, r, "", parts[2], namespace, parts[5], clients[clusterName])
+						clusterName, namespace := split(parts[4])
+						list(w, r, "", parts[2], clusterName, namespace, parts[5], clients[clusterName])
 					case 7:
-						clusterName, namespace := split(parts[5])
-						get(w, r, "", parts[2], namespace, parts[5], parts[6], clients[clusterName])
+						clusterName, namespace := split(parts[4])
+						get(w, r, "", parts[2], clusterName, namespace, parts[5], parts[6], clients[clusterName])
 					default:
-						done(w, nil, fmt.Errorf("unexpected number of path parts"))
+						serverError(w, fmt.Errorf("unexpected number of path parts"))
 					}
 				}
 			})
-			server := &http.Server{
-				Addr: ":8080",
-			}
+			server := &http.Server{Addr: ":2473"}
 			fmt.Printf("starting on %q\n", server.Addr)
 			return server.ListenAndServe()
 		},
@@ -113,21 +132,56 @@ func NewCommand() *cobra.Command {
 	return cmd
 }
 
+func create(w http.ResponseWriter, r *http.Request, group, version, clusterName, namespace, resource string, client dynamic.Interface) {
+	opts := metav1.CreateOptions{}
+	_ = decoder.Decode(&opts, r.URL.Query())
+	obj := &unstructured.Unstructured{}
+	err := json.NewDecoder(r.Body).Decode(obj)
+	if err != nil {
+		serverError(w, err)
+		return
+	}
+	obj.SetNamespace(namespace)
+	v, err := client.Resource(schema.GroupVersionResource{Group: group, Version: version, Resource: resource}).Namespace(namespace).Create(r.Context(), obj, opts)
+	if err != nil {
+		serverError(w, err)
+	} else {
+		v.SetNamespace(join(clusterName, namespace))
+		ok(w, v)
+	}
+}
+
+func join(clusterName, namespace string) string {
+	return fmt.Sprintf("%s.%s", clusterName, namespace)
+}
+
 func split(s string) (string, string) {
 	parts := strings.Split(s, ".")
 	return parts[0], parts[1]
 }
 
-func list(w http.ResponseWriter, r *http.Request, group, version, namespace, resource string, client dynamic.Interface) {
+func list(w http.ResponseWriter, r *http.Request, group, version, clusterName, namespace, resource string, client dynamic.Interface) {
 	opts := metav1.ListOptions{}
 	_ = decoder.Decode(&opts, r.URL.Query())
-	list, err := client.Resource(schema.GroupVersionResource{Group: group, Version: version, Resource: resource}).Namespace(namespace).List(r.Context(), opts)
-	done(w, list, err)
+	v, err := client.Resource(schema.GroupVersionResource{Group: group, Version: version, Resource: resource}).Namespace(namespace).List(r.Context(), opts)
+	if err != nil {
+		serverError(w, err)
+	} else {
+		for _, item := range v.Items {
+			item.SetNamespace(join(clusterName, namespace))
+		}
+		ok(w, v)
+	}
 }
 
-func get(w http.ResponseWriter, r *http.Request, group, version, namespace, resource, name string, client dynamic.Interface) {
+func get(w http.ResponseWriter, r *http.Request, group, version, clusterName, namespace, resource, name string, client dynamic.Interface) {
 	opts := metav1.GetOptions{}
 	_ = decoder.Decode(&opts, r.URL.Query())
 	v, err := client.Resource(schema.GroupVersionResource{Group: group, Version: version, Resource: resource}).Namespace(namespace).Get(r.Context(), name, opts)
-	done(w, v, err)
+	if err != nil {
+		serverError(w, err)
+	} else {
+		v.SetNamespace(join(clusterName, namespace))
+		ok(w, v)
+	}
 }
